c13ccba1f7fe7b301dd8bef13dc4d80f98350e1f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-11 16:30:28 +0000

    Deploy liuxinqiong/ui-demo to github.com/liuxinqiong/ui-demo.git:gh-pages

---
+++ precache-manifest.f61789ce3c5597ffc5742fe679d9cddb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a94823bf4f713d05ed",
+    "url": "0.0599bd53.iframe.bundle.js"
+  },
+  {
+    "revision": "26bec1915b3cc749b352",
+    "url": "4.7c8e7c38.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.7c8e7c38.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "4753e072a812852cbfc5",
+    "url": "5.d50bb783.iframe.bundle.js"
+  },
+  {
+    "revision": "483e3ed415368ef172bd",
+    "url": "6.5317b04f.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.5317b04f.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "963f3ce1b420c5875e06",
+    "url": "7.c3320a55.iframe.bundle.js"
+  },
+  {
+    "revision": "4b9c99cb65e8ce2aae1a4c327f09da0f",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "cfe21c35c798a923edfb",
+    "url": "main.631410b6.iframe.bundle.js"
+  },
+  {
+    "revision": "5fd5a1e33ba1dfdf1f4b",
+    "url": "runtime~main.f1485f73.iframe.bundle.js"
+  },
+  {
+    "revision": "cfe21c35c798a923edfb",
+    "url": "static/css/main.a7ad3634.chunk.css"
+  },
+  {
+    "revision": "a79f93438c19a53b0dfc",
+    "url": "vendors~main.a591bb18.iframe.bundle.js"
+  },
+  {
+    "revision": "19f59bd060ecd4d30f7b37c47fdc9b6b",
+    "url": "vendors~main.a591bb18.iframe.bundle.js.LICENSE.txt"
+  }
+]);
